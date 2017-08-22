a29d57ff58a9d33f6179724c033904b2d8951247
Felipe <felipeo510.fs@gmail.com>
2017-08-21 19:05:31 -0700

    print all names project

---
+++ namesOnly.js
@@ -0,0 +1,14 @@
+var arr = ["Chris","Juliana","Stephen","Felipe"];
+
+function allNames() {
+
+    var num = 0;
+
+for(var i = 0; i < arr.length; i++) {
+
+    console.log(num + " -> "+ arr[i]);
+    num++;
+
+    }
+}
+allNames();
